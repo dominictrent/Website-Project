3a22cbfa99808a313e32095f93aec36d1960113a
dominictrent <Dominctrent@icloud.com>
2022-12-02 11:11:30 -0700

    idk

---
+++ quiz.js
@@ -0,0 +1,13 @@
+document.getElementById("Submit").addEventListener("click", grade);
+
+function grade() {
+  let Q1V = Q1;
+  let Q2I = Q2;
+  let Q3M = Q3;
+  let Q4H = Q4;
+  let VIMH = (Q1V + Q2I + Q3M + Q4H) / 5;
+
+  if (Q1V )
+
+  document.getElementById("Submit").innerHTML = `Submit test ${AsnwerID}%.`;
+}
