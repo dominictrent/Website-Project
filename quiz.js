6b596b8ab6d6897ecb8a1f487805765807eb3995
dominictrent <Dominctrent@icloud.com>
2022-12-05 11:23:57 -0700

    getting there

--- quiz.js
+++ quiz.js
@@ -1,16 +1,41 @@
-document.getElementById("Submit").addEventListener("click", grade);
+document.getElementById("SubmitTest").addEventListener("click", SubmitTest);
 
-function grade() {
-  let Q1V = Q1;
-  let Q2I = Q2;
-  let Q3M = Q3;
-  let Q4H = Q4;
-  let VIMH = (Q1V + Q2I + Q3M + Q4H) / 5;
-
-  if (Q1V === "Visors help portect the face and protect the eyes") {
-    AsnwerID = "correct";
+function SubmitTest() {
+  // Check Question 1
+  let Q1ST = document.getElementById("Visor").value;
+  if (Q1ST === "Protect the eyes" || Q1ST === "Protect the eys and face") {
+    document.getElementById("q1-response").innerHTML = "Correct";
+  } else {
+    document.getElementById("q1-response").innerHTML = "Incorrect";
   }
 
-  if (Q1V)
-    document.getElementById("Submit").innerHTML = `Submit test ${VIMH}%.`;
+  let Q2ST = document.getElementById("Helmet").value;
+  let Q3ST = document.getElementById("Mouth").value;
+  let Q4ST = document.getElementById("Safe").value;
+
+  if (Q2ST === "Chinstrap" || Q2ST === "The Chinstrap") {
+    console.log;
+    Q2ST = 1;
+  } else {
+    Q2ST = 0;
+  }
+  if (Q3ST === "Protect the mouth" || Q3ST === "Protect the mouth and jaw") {
+    console.log;
+    Q3ST = 1;
+  } else {
+    Q3ST = 0;
+  }
+  if (
+    Q4ST === "Padding" ||
+    Q4ST === "The Padding and aired up foam inside the helemt"
+  ) {
+    Q4ST = 1;
+  } else {
+    Q4ST = 0;
+  }
+  let total = (Q1ST + Q2ST + Q3ST + Q4ST) / 4;
+  let total1 = total;
+
+  // document.getElementById("total").innerHTML = `Final Grade: ${Average}%.`;
 }
+document.getElementById("total").addEventListener("click", SubmitTest);
